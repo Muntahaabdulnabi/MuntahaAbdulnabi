584bffffcc30b5428e00bc666aef68b1380109e4
muntahaabdulnabi <muntahaabdulnabi@hotmail.com>
2022-11-28 11:07:31 +0100

    CustomerController added

---
+++ WebApi/Models/CustomerCreateModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi.Models
+{
+    public class CustomerCreateModel
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Phone { get; set; }
+        public string Address { get; set; } = null!;
+        public string? CustomerName => $"{FirstName} {LastName}";
+    }
+}
