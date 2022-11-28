88f6c05e835d7194908faebf81ff8e7d00c44a8e
muntahaabdulnabi <muntahaabdulnabi@hotmail.com>
2022-11-28 10:19:38 +0100

    DataContext fixed

---
+++ WebApi/Contexts/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models.Entities;
+
+namespace WebApi.Contexts
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CustomerEntity> Customers { get; set; }
+        public DbSet<ProductEntity> Products { get; set; }
+        public DbSet<OrderEntity> Orders { get; set; }
+    }
+}
