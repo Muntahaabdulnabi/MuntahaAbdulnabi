8c911633f153738deec1727fc141a1261c8d55c3
muntahaabdulnabi <muntahaabdulnabi@hotmail.com>
2022-12-02 11:47:11 +0100

    Jag måste lämna in den som den är. Annars kommer jag ha hjärtattack för att jag kan inte slippa tänka på den en enda minut 24/7. Väldigt Tacksam för ditt tålamod!

--- WebApi/Models/Entities/CustomerEntity.cs
+++ WebApi/Models/Entities/CustomerEntity.cs
@@ -1,4 +1,6 @@
-﻿namespace WebApi.Models.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models.Entities;
 
 public class CustomerEntity
 {
@@ -10,3 +12,8 @@ public class CustomerEntity
     public string Address { get; set; } = null!;
     public string CustomerName => $"{FirstName} {LastName}";
 }
+
+
+// Jag kunde ha lagt Data Annotation som t.ex. [Key], [Required] och[StringLength(max)] [Column(DataType)],
+// men det inte så viktigt eftersom vissa är redan finns genom att sätta "Id" och " =nul!".
+// MaxLength är inte så viktig i detta projekt.
